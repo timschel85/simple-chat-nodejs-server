03db80fa6cbb3c5097f77f0e7438075905c49948
Charles Kim <timschel85@gmail.com>
2017-05-31 00:18:27 +0900

    채팅 메시지를 입력하면 스크롤 됨. CSS파일 적용.

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const path = require('path')
+const config = require('./config')
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '/public/css')));
+
+io.on('connection', (socket)=>{
+    console.log('A Client has connected.')
+
+    socket.on('fromclient', (data)=>{
+        console.log("[From Client] : " + data)
+        socket.emit('toclient', data) // send the message to the client who send the message
+        socket.broadcast.emit('toclient', data)
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log('A Client has disconnected.')
+    })
+})
+
+app.get('/', (req, res)=>{
+    res.render('chatroom')
+
+})
+
+http.listen(config.port, ()=>{
+    console.log('Listening to the port number ' + config.port)
+})
